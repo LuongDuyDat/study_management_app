6ebf60e70418e15a1aad0fc7ee2083ea21669f8f
LuongDuyDat <luongduydat123@gmail.com>
2023-12-18 11:35:57 +0700

    add route api v1

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Http\Controllers\AssignmentController;
+use App\Http\Controllers\ExerciseFileController;
+use App\Http\Controllers\LectureController;
+use App\Http\Controllers\LecturerController;
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\StudentExerciseController;
+use App\Http\Controllers\SubjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +21,48 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function() {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
+
+Route::controller(AssignmentController::class)->group(function() {
+    Route::get('/assignments/{assignment}/exercises', 'getExercises');
+    Route::apiResources('assignments');
+});
+
+Route::controller(ExerciseFileController::class)->group(function() {
+    Route::apiResources('exercises_files');
+});
+
+Route::controller(LectureController::class)->group(function() {
+    Route::apiResources('lectures');
+});
+
+Route::controller(LecturerController::class)->group(function() {
+    Route::get('/lecturers/{lecturer}/subjects', 'getSubjects');
+    Route::get('/lecturers/{lecturer}/assignments', 'getAssignments');
+    Route::get('/lecturers/{lecturer}/lectures', 'getLectures');
+    Route::apiResources('lecturers');
+});
+
+Route::controller(StudentController::class)->group(function() {
+    Route::get('/students/{student}/subjects', 'getSubjects');
+    Route::get('/students/{student}/exercises', 'getExercises');
+    Route::get('/students/{student}/assignments', 'getAssignments');
+    Route::apiResources('students');
+});
+
+Route::controller(StudentExerciseController::class)->group(function() {
+    Route::get('/exercises/{student_exercise}/exercise_files', 'getExercisesFile');
+    Route::get('/exercises/{student_exercise}/assignment', 'getAssignment');
+    Route::apiResources('exercises');
+});
+
+Route::controller(SubjectController::class)->group(function() {
+    Route::get('/subjects/{subject}/students', 'getStudents');
+    Route::get('/subjects/{subject}/lecturer', 'getLecturer');
+    Route::get('/subjects/{subject}/assignments', 'getAssignments');
+    Route::get('/subjects/{subject}/lectures', 'getLectures');
 });
